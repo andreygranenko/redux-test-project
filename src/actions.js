57e7207c92394e49483a3e17ce5bedfaca9e0ad7
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-03-08 22:03:40 +0200

    test branch

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,5 @@
 export const inc = () => ({ type: 'INCREMENT' });
 export const dec = () => ({type: 'DECREMENT'});
 export const rnd = () => ({type: 'RANDOM', payload: Math.floor(Math.random() * 10)})
+
+console.log('testing stuff');
